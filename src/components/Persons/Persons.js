293e367bc261b68925ac71a0027db3cc5382ef9e
sarthak19-qait <sarthaksaxena@qainfotech.com>
2019-11-03 13:29:05 +0530

    dsadsd

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Person from './Person/Person';
+
+class Persons extends Component {
+  constructor(props){
+    super(props)
+    console.log("[Persons.js] constructor")
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[Persons.js] getDerivedStateFromProps', props);
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('[Persons.js] componentDidMount');
+  }
+  render(){
+  console.log('[Persons.js] rendering...');
+  return this.props.persons.map((person, index) => {
+    return (
+      <Person
+        click={() => this.props.clicked(index)}
+        name={person.name}
+        age={person.age}
+        key={person.id}
+        changed={event => this.props.changed(event, person.id)}
+      />
+    );
+  });
+}
+}
+
+export default Persons;
